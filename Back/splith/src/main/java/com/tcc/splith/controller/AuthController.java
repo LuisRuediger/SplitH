54546087d70da23c6797b403c8c30c0955af4340
Cristiano Heinzen <critiano.heinzen@gmail.com>
2025-11-17 22:32:56 -0300

    FEAT - AuthConfig

--- Back/splith/src/main/java/com/tcc/splith/controller/AuthController.java
+++ Back/splith/src/main/java/com/tcc/splith/controller/AuthController.java
@@ -8,8 +8,13 @@ import com.tcc.splith.dto.response.RegisterUserResponse;
 import com.tcc.splith.entity.User;
 import com.tcc.splith.repository.UserRepository;
 import jakarta.validation.Valid;
+import org.springframework.boot.autoconfigure.pulsar.PulsarProperties;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -18,19 +23,32 @@ public class AuthController {
 
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
 
-    public AuthController(UserRepository userRepository) {
+    public AuthController(UserRepository userRepository, PasswordEncoder passwordEncoder, AuthenticationManager authenticationManager) {
+        this.passwordEncoder = passwordEncoder;
         this.userRepository = userRepository;
+        this.authenticationManager = authenticationManager;
     }
 
     @PostMapping("/login")
     public ResponseEntity<LoginResponse> login(@Valid @RequestBody LoginRequest request){
+
+        UsernamePasswordAuthenticationToken userAndPass = new UsernamePasswordAuthenticationToken(
+                request.email(),
+                request.password()
+        );
+        Authentication authentication = authenticationManager.authenticate(userAndPass);
+
+
+
         return null;
     }
 
     public ResponseEntity <RegisterUserResponse> register(@Valid @RequestBody RegisterUserRequest request) {
         User newUser = new User();
-        newUser.setPassword(request.password());
+        newUser.setPassword(passwordEncoder.encode(request.password()));
         newUser.setEmail(request.email());
         newUser.setName(request.name());
 
