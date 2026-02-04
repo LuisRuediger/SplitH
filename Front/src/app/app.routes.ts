87a3938316d45be7bea32c76f7b4b120ba707a4c
Cristiano Heinzen <cristiano.heinzen@gmail.com>
2026-02-04 18:53:56 -0300

    Criando rotas iniciais

--- Front/src/app/app.routes.ts
+++ Front/src/app/app.routes.ts
@@ -1,3 +1,21 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
-export const routes: Routes = [];
+// 1. Importe os seus componentes aqui
+import { LoginForm } from './components/login-form/login-form';
+import { Dashboard } from './components/dashboard/dashboard';
+
+const routes: Routes = [
+  // 2. Defina qual componente aparece em cada "endereço"
+  { path: 'login', component: LoginForm },
+  { path: 'dashboard', component: Dashboard },
+  
+  // 3. Rota padrão: Se o usuário não digitar nada, vai para o login
+  { path: '', redirectTo: '/login', pathMatch: 'full' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
