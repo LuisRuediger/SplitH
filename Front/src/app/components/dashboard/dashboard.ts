3409caed86eb1eb5e4a8f7916d13c9ebbbeaef87
Cristiano Heinzen <critiano.heinzen@gmail.com>
2026-02-09 22:08:46 -0300

    feat: adicionado componente MENU PrimeNG ao Dashboard

--- Front/src/app/components/dashboard/dashboard.ts
+++ Front/src/app/components/dashboard/dashboard.ts
@@ -1,11 +1,68 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+// Importações do PrimeNG
+import { MenuModule } from 'primeng/menu';
+import { BadgeModule } from 'primeng/badge';
+import { AvatarModule } from 'primeng/avatar';
+import { RippleModule } from 'primeng/ripple';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  standalone: true,
+  // Importante: Todos os módulos que o HTML usa devem estar aqui
+  imports: [CommonModule, MenuModule, BadgeModule, AvatarModule, RippleModule],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css',
 })
-export class Dashboard {
+export class Dashboard implements OnInit { 
+  items: MenuItem[] | undefined;
 
+  ngOnInit() {
+        this.items = [
+            {
+                separator: true
+            },
+            {
+                label: 'Documents',
+                items: [
+                    {
+                        label: 'New',
+                        icon: 'pi pi-plus',
+                        shortcut: '⌘+N'
+                    },
+                    {
+                        label: 'Search',
+                        icon: 'pi pi-search',
+                        shortcut: '⌘+S'
+                    }
+                ]
+            },
+            {
+                label: 'Profile',
+                items: [
+                    {
+                        label: 'Settings',
+                        icon: 'pi pi-cog',
+                        shortcut: '⌘+O'
+                    },
+                    {
+                        label: 'Messages',
+                        icon: 'pi pi-inbox',
+                        badge: '2'
+                    },
+                    {
+                        label: 'Logout',
+                        icon: 'pi pi-sign-out',
+                        shortcut: '⌘+Q',
+                        linkClass: '!text-red-500 dark:!text-red-400'
+                    }
+                ]
+            },
+            {
+                separator: true
+            }
+        ];
+    }
 }
