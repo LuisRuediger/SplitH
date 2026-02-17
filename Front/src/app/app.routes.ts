c5a1c0291cea94eb7078b57186f76d7c497681fc
Luis Ruediger <luisruediger27@gmail.com>
2026-02-17 16:14:51 -0300

    feat(user-register): add registration component and route

--- Front/src/app/app.routes.ts
+++ Front/src/app/app.routes.ts
@@ -3,12 +3,14 @@ import { Routes } from '@angular/router';
 // 1. Importação dos componentes
 import { LoginForm } from './components/login-form/login-form';
 import { Dashboard } from './components/dashboard/dashboard';
+import { UserRegister } from './components/user-register/user-register';
 
 // 2. Exportação direta da constante 'routes'
 export const routes: Routes = [
   { path: 'login', component: LoginForm },
+  { path: 'register', component: UserRegister },
   { path: 'dashboard', component: Dashboard },
-  
+
   // Rota padrão (sem a barra inicial no redirectTo para evitar erros em algumas versões)
   { path: '', redirectTo: 'login', pathMatch: 'full' }
 ];
