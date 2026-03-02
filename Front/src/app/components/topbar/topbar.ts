ca495dd57a68ec1063362b4413700571d6f7a851
Cristiano Heinzen <critiano.heinzen@gmail.com>
2026-03-01 21:32:34 -0300

    UPDATE - alterando estilos da topbar e aside

--- Front/src/app/components/topbar/topbar.ts
+++ Front/src/app/components/topbar/topbar.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { ToolbarModule } from 'primeng/toolbar';
 import { AvatarModule } from 'primeng/avatar';
+import { ButtonModule } from 'primeng/button';
+
 
 @Component({
   selector: 'app-topbar',
   standalone: true,
-  imports: [ToolbarModule, AvatarModule],
+  imports: [ToolbarModule, AvatarModule, ButtonModule],
   templateUrl: './topbar.html',
   
 })
@@ -13,3 +15,6 @@ export class Topbar {
 // Variável para armazenar o nome do usuário virá do BACKEND
   nomeUsuario: string = 'Cris';
 }
+
+export class ToolbarCustomDemo {}
+
