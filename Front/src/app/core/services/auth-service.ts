34e351cc8d4288c969d8618693c571c2d5421291
Luis Ruediger <luisruediger27@gmail.com>
2026-03-03 17:03:56 -0300

    feat(auth): implement JWT authentication service

---
+++ Front/src/app/core/services/auth-service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { tap } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  private api = 'http://localhost:8080/auth';
+
+  constructor(private http: HttpClient, private router: Router) {};
+
+  login(email: string, password: string) {
+    return this.http.post<{ token: string }>(`${this.api}/login`, { email, password }).pipe(tap(res => localStorage.setItem('token', res.token)))
+  }
+
+}
