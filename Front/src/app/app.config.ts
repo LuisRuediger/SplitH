ad97789ae62bf43c88a90d9d46c2acf61d2d36e3
Luis Ruediger <luisruediger27@gmail.com>
2026-03-06 14:17:07 -0300

    feat(login): add authInterceptor

--- Front/src/app/app.config.ts
+++ Front/src/app/app.config.ts
@@ -4,13 +4,14 @@ import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeuix/themes/aura';
 import { provideRouter } from '@angular/router'; // 1. Importe o provideRouter
 import { routes } from './app.routes'; // 2. Importe o arquivo de rotas que criamos
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { authInterceptor } from './core/services/auth-interceptor';
 
 export const appConfig: ApplicationConfig = {
     providers: [
         provideRouter(routes), // 3. Adicione aqui para ativar a navegação!
         provideAnimationsAsync(),
-        provideHttpClient(),
+        provideHttpClient(withInterceptors([authInterceptor])),
         providePrimeNG({
 
           theme: {
